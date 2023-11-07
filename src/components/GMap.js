2e3e1b5acc5c78e8d185d378400de1c517f33487
dhungwt <112122672+dhungwt@users.noreply.github.com>
2023-11-07 17:08:09 -0500

    map styling added, satellite set as default

--- src/components/GMap.js
+++ src/components/GMap.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { GoogleMap, LoadScript, TrafficLayer } from "@react-google-maps/api";
 
 function GMap() {
   const [map, setMap] = useState(null);
 
   const mapContainerStyle = {
     width: "100%",
-    height: "400px",
+    height: "700px",
   };
 
   const center = {
-    lat: 40.630494495970645, 
-    lng: -73.95242628063484,
+    lat: 40.6305372434552, 
+    lng: -73.95243164504681,
   };
 
   const onLoad = (map) => {
@@ -26,8 +26,16 @@ function GMap() {
         mapContainerStyle={mapContainerStyle}
         center={center}
         zoom={28}
+        mapTypeId= "satellite"
         onLoad={onLoad}
-      />
+        gestureHandling= "none"
+        zoomControl= "false"
+        >
+          <TrafficLayer />
+        </GoogleMap>
+
+        
+
     </LoadScript>
   );
 }
